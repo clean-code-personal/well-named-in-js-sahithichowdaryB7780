09d5c3b1d5e0f70a739f3243ef600c214dd16b24
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-06 05:25:06 +0530

    Update colorCoder.js

--- app/colorCoder.js
+++ app/colorCoder.js
@@ -2,31 +2,24 @@ function ColorPair(){
            this.majorColor;
            this.minorColor;
 }
-
 ColorPair.prototype.toString=function(){
 	return `MajorColor:${this.majorColor},MinorColr:${this.minorColor}`;
 }
-
-function getColorFromPairNumber(pairNumber)
-{
+function getColorFromPairNumber(pairNumber){
 	const minorSize = MajorColorNames.length;
 	const majorSize = MinorColorNames.length;
 	
-	if (pairNumber < 1 || pairNumber > minorSize * majorSize)
- 	{
- 		throw `Argument PairNumber:${pairNumber} is outside the allowed range` 
-	}
+	if (pairNumber < 1 || pairNumber > minorSize * majorSize) {
+ 	
+ 		throw `Argument PairNumber:${pairNumber} is outside the allowed range` }
 	const zeroBasedPairNumber = pairNumber - 1;
     const majorIndex = parseInt (zeroBasedPairNumber / minorSize);
     const minorIndex = parseInt(zeroBasedPairNumber % minorSize);
 	let  pair = new ColorPair();
 	pair.majorColor = MajorColorNames[majorIndex];
 	pair.minorColor = MinorColorNames[minorIndex];
-	return pair;
-}
-
-function getPairNumberFromColor(pair)
-        {
+	return pair;}
+function getPairNumberFromColor(pair){
         let majorIndex = -1;
         for (let i = 0; i < MajorColorNames.length; i++)
             {
@@ -51,8 +44,7 @@ function getPairNumberFromColor(pair)
             {
                 throw `Unknown Colors:${pair.toString()}`;
             }
-
-        return (majorIndex * MinorColorNames.length) + (minorIndex + 1);
-	 }
+return (majorIndex * MinorColorNames.length) + (minorIndex + 1);}
+	 
 	 
 
